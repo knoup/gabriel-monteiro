d910979198928035b7a5a9d66f6e892c54a1c96a
Pedro Manoel <dev.pedromanoel@pm.me>
2021-03-04 03:55:35 -0300

    Command categories and Required arguments

--- index.js
+++ index.js
@@ -5,6 +5,15 @@ const { prefix, token } = require('./config.json');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
 
+const commandFolders = fs.readdirSync('./commands');
+
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const file of commandFiles) {
+        const command = require(`./commands/${folder}/${file}`);
+        client.commands.set(command.name, command);
+    }
+}
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -28,6 +37,10 @@ client.on('message', message => {
     if (!client.commands.has(commandName)) return;
 
     const command = client.commands.get(commandName);
+
+    if (command.args && !args.length) {
+        return message.channel.send(`Você não forneceu nenhum argumento, ${message.author}`);
+    }
 
     try {
         command.execute(message, args);

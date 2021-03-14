b77854bd25a8c214f36295bb10f0bb6b72be7f27
Pedro Manoel <dev.pedromanoel@pm.me>
2021-03-14 13:41:25 -0300

    increase the embed messages

--- commands/utility/play.js
+++ commands/utility/play.js
@@ -21,15 +21,22 @@ const execute = async (client, message, args) => {
         const videos = result.videos.slice(0, 1); // Taking only the first video from response and adding into a array
         videos.forEach(function(video) {
             // message.reply(`${video.url}`); // Replying in chat the song's URL
-            return songUrl = video.url, songTitle = video.title, songThumb = video.thumbnail, songDuration = video.timestamp, songViews = video.views, songAgo = video.ago; // Assign the song's URL to a variable
+            return songUrl = video.url, songTitle = video.title, songThumb = video.image, songDuration = video.timestamp, songViews = video.views, songAgo = video.ago; // Assign the song's URL to a variable
         });
         const queue = client.queues.get(message.guild.id);
         if (queue) { // If a queue already exists
-            console.log('ENTREI NA LINHA 27');
             queue.songs.push(songUrl); // push the new typed song on the queue
             client.queues.set(message.guild.id, queue);
+            let messageBanner = new MessageEmbed()
+                .setAuthor('MÚSICA ADICIONADA À FILA! ✅', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
+                .setThumbnail(songThumb)
+                .setColor('GREEN')
+                .addField('Nome:', songTitle)
+                .addField('Duração:', songDuration, true)
+                .addField('Pedida por:', message.member, true)
+                .setFooter(`${songViews} visualizações | ${songAgo}`);
+            return message.channel.send(messageBanner);
         } else {
-            console.log('entrei na linha 31');
             playSong(client, message, songUrl); // If not exists a queue, the playSong function is called
             let messageBanner = new MessageEmbed()
                 .setAuthor('TOCANDO AGORA! 🔊', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
@@ -60,15 +67,16 @@ const playSong = async (client, message, songUrl) => {
     }
     queue.dispatcher = await queue.connection.play(await ytdl(songUrl), { highWaterMark: 1 << 25, type: 'opus' });
     queue.dispatcher.on('finish', () => {
-        console.log(`before shift -> ${queue.songs}`);
         queue.songs.shift(); // Removing the first elements from queue(array)
-        console.log(`after shift -> ${queue.songs}`);
-        console.log('playing new song from queue');
         if (queue.songs[0]) {
-            console.log('ENTREI NA LINHA 59');
             playSong(client, message, queue.songs[0]); // Playing the first song from queue
+            let messageBanner = new MessageEmbed()
+                .setAuthor('TOCANDO AGORA! 🔊', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
+                .setThumbnail(songThumb)
+                .setColor('BLUE')
+                .addField('Link:', queue.songs[0]);
+            return message.channel.send(messageBanner);
         } else {
-            console.log('ENTREI NA LINHA 62');
             return client.queues.delete(message.member.guild.id); // As has no song on queue, it's deleted
         }
     });

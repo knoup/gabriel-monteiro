ff47342698b87ac09b8e704bf95106f421321fc1
Pedro Manoel <dev.pedromanoel@pm.me>
2021-03-17 14:13:57 -0300

    💡 Adding more comments

--- commands/utility/play.js
+++ commands/utility/play.js
@@ -14,28 +14,26 @@ const execute = async (client, message, args) => {
         // Verifying if the user is connected to a voice channel
         if (!message.member.voice.channel) return message.reply('você precisar estar em um canal do voz.');
 
-        //
-        // const botUser = new Discord.User();
-
         // Searching for URL on YouTube
         const songRequested = args.join(' '); // Taking the song's name/URL from command
         // let songUrl; // Creating a variable to receive the song's URL
         const result = await yts(songRequested); // Searching for the song's name/URL
         const videos = result.videos.slice(0, 1); // Taking only the first video from response and adding into a array
         const video = videos[0];
-        // videos.forEach(function(video) {
-        //     // message.reply(`${video.url}`); // Replying in chat the song's URL
-        //     return video;
-        //     // return songUrl = video.url, video.title = video.title, video.image = video.image, video.duration = video.timestamp, video.views = video.views, video.ago = video.ago; // Assign the song's URL to a variable
-        // });
         const queue = client.queues.get(message.guild.id);
-        if (queue) { // If a queue already exists
+
+        // If a queue already exists
+        if (queue) {
             queue.songs.push(video); // push the new typed song on the queue
-            client.queues.set(message.guild.id, queue);
+            client.queues.set(message.guild.id, queue); // Using a queue from that server
+
+            // Verifying if the last message has a title specific
             if (!client.user.lastMessage.embeds[0].title === 'TOCANDO AGORA! 🔊') {
                 client.user.lastMessage.delete();
             }
-            message.delete();
+
+            message.delete(); // Deleting the last user message
+
             const songAdded = new MessageEmbed()
                 .setAuthor('MÚSICA ADICIONADA À FILA! ✅', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
                 .setThumbnail(video.image)
@@ -45,12 +43,17 @@ const execute = async (client, message, args) => {
                 .addField('Pedida por:', message.member, true)
                 .setFooter(`${video.views} visualizações | ${video.ago}`);
             return message.channel.send(songAdded);
+
         } else {
             try {
-                if (client.user.lastMessage) client.user.lastMessage.delete();
-                message.channel.bulkDelete(10);
+                if (client.user.lastMessage) client.user.lastMessage.delete(); // Verifying if the client sent a message before that session
+
+                message.channel.bulkDelete(10); // Deleting the last 10 messages to cluan up the channel
+
                 playSong(client, message, video); // If not exists a queue, the playSong function is called
-                client.user.setActivity(`${video.title}`, { type: 'LISTENING' });
+
+                client.user.setActivity(`${video.title}`, { type: 'LISTENING' }); // Setting the song name from status bot
+
                 const messageBanner = new MessageEmbed()
                     .setAuthor('', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
                     .setTitle('TOCANDO AGORA! 🔊')
@@ -61,6 +64,7 @@ const execute = async (client, message, args) => {
                     .addField('Pedida por:', message.member, true)
                     .setFooter(`${video.views} visualizações | ${video.ago}`);
                 return message.channel.send(messageBanner);
+
             } catch {
                 console.log('error!');
             }
@@ -73,7 +77,7 @@ const execute = async (client, message, args) => {
 const playSong = async (client, message, video) => {
     let queue = client.queues.get(message.member.guild.id);
     if (!queue) { // If has no queue, create one
-        const conn = await message.member.voice.channel.join();
+        const conn = await message.member.voice.channel.join(); // Joining the voice channel
         queue = {
             volume: 10,
             connection: conn,
@@ -81,14 +85,20 @@ const playSong = async (client, message, video) => {
             songs: [video],
         };
     }
-    queue.dispatcher = await queue.connection.play(await ytdl(video.url), { highWaterMark: 1 << 25, type: 'opus', filter: 'audioonly', quality: 'highestaudio' });
-    queue.dispatcher.on('finish', () => { // When the music is finish
+    queue.dispatcher = await queue.connection.play(await ytdl(video.url), { highWaterMark: 1 << 25, type: 'opus', filter: 'audioonly', quality: 'highestaudio' }); // Playing the song
+
+    // When the music is finish
+    queue.dispatcher.on('finish', () => {
         queue.songs.shift(); // Removing the first elements from queue(array)
-        if (queue.songs[0]) { // Verifying if has song on queue
-            // message.delete();
+
+        // Verifying if has song on queue
+        if (queue.songs[0]) {
             message.channel.bulkDelete(10); // Deleting the last message from bot
+
             playSong(client, message, queue.songs[0]); // Playing the first song from queue
-            client.user.setActivity(`${queue.songs[0].title}`, { type: 'LISTENING' });
+
+            client.user.setActivity(`${queue.songs[0].title}`, { type: 'LISTENING' }); // Setting the song name from status bot
+
             const nowPlaying = new MessageEmbed()
                 .setAuthor('', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
                 .setTitle('TOCANDO AGORA! 🔊')
@@ -99,14 +109,16 @@ const playSong = async (client, message, video) => {
                 .addField('Pedida por:', message.member, true)
                 .setFooter(`${queue.songs[0].views} visualizações | ${queue.songs[0].ago}`);
             return message.channel.send(nowPlaying);
+
         } else {
-            client.user.lastMessage.delete();
-            console.log(client.user.lastMessage.embeds[0].title);
-            message.member.voice.channel.leave();
+            client.user.lastMessage.delete(); // Deleting the last bot message
+
+            message.member.voice.channel.leave(); // Leaving the voice channel
+
             const nothingPlaying = new MessageEmbed()
                 .setAuthor('FILA VAZIA! 🔇', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
                 .setColor('RED');
-            client.user.setActivity('a esquerda chorar.', { type: 'LISTENING' });
+            client.user.setActivity('a esquerda chorar.', { type: 'LISTENING' }); // Setting the song name from status bot
             return message.channel.send(nothingPlaying), client.queues.delete(message.member.guild.id);
         }
     });

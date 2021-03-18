93aec443a05b7d2e50515458fc72e9c2a00b754e
Pedro Manoel <dev.pedromanoel@pm.me>
2021-03-18 14:04:38 -0300

    💡chore(comments/messageEmbeds): Add comments to code and set the message embed

--- commands/utility/skip.js
+++ commands/utility/skip.js
@@ -7,52 +7,50 @@ const execute = async (client, message) => {
         // Verifying if the user is connected to a voice channel
         if (!message.member.voice.channel) return message.reply('você precisar estar em um canal do voz.');
 
-        const queue = client.queues.get(message.guild.id);
+        const queue = client.queues.get(message.guild.id); // Getting current queue
 
-        if (!queue) return message.reply('Nenhuma música está tocando.');
+        if (!queue) return message.reply('Nenhuma música está tocando.'); // Verifying if a song is playing
 
         queue.songs.shift(); // Removing the first elements from queue(array)
         if (queue.songs[0]) {
             const skiped = new MessageEmbed()
-                .setAuthor('MÚSICA SKIPADA! ⏩', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
+                .setTitle('MÚSICA SKIPADA! ⏩')
                 .setColor('RED')
-                .addField('Por:', message.member);
+                .setDescription(`Por: ${message.member}`);
             message.channel.send(skiped);
 
             message.channel.bulkDelete(10); // Deleting the last 10 messages from channel
 
-            client.queues.set(message.guild.id, queue);
+            client.queues.set(message.guild.id, queue); // Updating queue
 
             client.user.setActivity(`${queue.songs[0].title}`, { type: 'LISTENING' }); // Setting the song name from status bot
 
-            playSong(client, message, queue.songs[0]);
+            playSong(client, message, queue.songs[0]); // Playing the new queue's first song 
 
             const nowPlaying = new MessageEmbed()
-                .setAuthor('', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
                 .setTitle('TOCANDO AGORA! 🔊')
                 .setThumbnail(queue.songs[0].image)
                 .setColor('BLUE')
                 .addField('Nome:', queue.songs[0].title)
-                .addField('Duração:', queue.songs[0].duration, true)
+                .addField('Duração:', queue.songs[0].timestamp, true)
                 .addField('Pedida por:', message.member, true)
                 .setFooter(`${queue.songs[0].views} visualizações | ${queue.songs[0].ago}`);
             return message.channel.send(nowPlaying);
         } else {
-            client.user.lastMessage.delete(); // Deleting the last bot message
+            message.channel.bulkDelete(10); // Deleting the 10 last messages from channel
 
             message.member.voice.channel.leave(); // Leaving the voice channel
 
             const nothingPlaying = new MessageEmbed()
-                .setAuthor('FILA VAZIA! 🔇', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQuulkKdCSVtNZ60bIRYRuOqv2452Gpo1Qtxg&usqp=CAU')
-                .setColor('RED');
+                .setTitle('FILA VAZIA! 🔇')
+                .setColor('RED')
+                .setDescription('Digite `!play <nome/link>` para tocar uma música.');
             client.user.setActivity('a esquerda chorar.', { type: 'LISTENING' }); // Setting the song name from status bot
             return message.channel.send(nothingPlaying), client.queues.delete(message.member.guild.id);
         }
-
     } catch (error) {
         console.log(error);
     }
-
 };
 module.exports = {
     name: 'skip',

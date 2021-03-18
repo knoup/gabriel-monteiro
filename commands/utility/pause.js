0d648826f884513371211c70009bc91163a73c4b
Pedro Manoel <dev.pedromanoel@pm.me>
2021-03-18 13:28:44 -0300

    🚧chore: Use the attribute 'playing' from 'queue' object and add a condition to delete the bot's lastMessage

--- commands/utility/pause.js
+++ commands/utility/pause.js
@@ -1,19 +1,26 @@
 const { MessageEmbed } = require('discord.js');
-
 const execute = (client, message) => {
     const queue = client.queues.get(message.guild.id); // Create a instance queue
 
-    if (!queue) return message.reply('Não há nenhumaa música sendo tocada!'); // Verify if a queue exist
+    if (!queue) return message.reply('não há nenhuma música sendo tocada!'); // Verify if a queue exist
 
     message.delete(); // Delete the user command
+    
+    if (!queue.playing) return message.reply('uma música já está pausada.'); // Verify if a song is playing
+
+    // Verifying if the last message has a title specific
+    if (client.user.lastMessage.embeds[0].title !== 'TOCANDO AGORA! 🔊') {
+        client.user.lastMessage.delete();
+    }
 
     queue.connection.dispatcher.pause(); // Pause the song
 
+    queue.playing = false; // changing playing state
+
     const songPaused = new MessageEmbed()
-        .setAuthor('MÚSICA PAUSADA! ⏸️ ')
+        .setTitle('MÚSICA PAUSADA! ⏸️ ')
         .setColor('#FF8C00')
-        .addField('Por:', message.member)
-        .setDescription('Digite `!resume` para retomar.');
+        .setDescription('Digite `!resume` para retomar');
     return message.channel.send(songPaused);
 };
 module.exports = {
